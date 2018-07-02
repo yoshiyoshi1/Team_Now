9336642d5d342decf1fe767558ec361c045d4e88
yoshiyoshi1 <ysdsiy0124@gmail.com>
2018-07-02 11:15:37 +0900

    07.02 Add Camera

---
+++ src/Camera.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#include "Camera.h"
+
+Camera::Camera()
+{
+
+}
+
+Camera::~Camera()
+{
+
+}
---
+++ src/Camera.h
@@ -0,0 +1,15 @@
+#ifndef __Camera_h__
+#define __Camera_h__
+
+class Camera
+{
+public :
+	Camera();
+	~Camera();
+
+private:
+
+};
+
+
+#endif
